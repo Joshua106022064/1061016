2910d5aeeb45cc1c6585dfd338922e4e5f32802c
unknown <joshua310041@gmail.com>
2017-10-16 15:28:48 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        float a=in.nextFloat();
+        float b=in.nextFloat();
+        double x;
+        x=Math.PI*a*a*b;
+        System.out.println(x);
+    }
+}
